e5f2e13744015d716ba64c9d35107dbc9cf0d11c
Francisco Azevedo <estgl3264@estgl.ipv.pt>
2018-11-16 19:16:46 +0000

    CI code coverage test

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,11 +1,9 @@
-/**
+﻿/**
  * @author João Santos <up201809187@fe.up.pt>
  */
 
-const sum = (a, b) => {
-    return a + b;
-}
+const sum = require('../utils/sum');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
+   expect(sum(1, 2)).toBe(3)
 });
---
+++ src/utils/sum.js
@@ -0,0 +1,8 @@
+﻿/**
+ * @author João Santos <up201809187@fe.up.pt>
+ */
+
+function sum(a, b) {
+   return a + b;
+}
+module.exports = sum;
